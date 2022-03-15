a13439d7dcc14e10624a97d946d6e32e334a8966
David Westenhaver <david.westenhaver74@journalism.cuny.edu>
2022-03-14 21:06:11 -0400

    Second Commit w/Proper JS Terms and Byline

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 const select = document.querySelector('.filter');
 
 select.addEventListener('change', function (e) {
-    const chosen_food = e.target.value;
+    const chosen_filter = e.target.value;
     const currently_visible = document.querySelectorAll('.item.active')
-    const make_visible = document.querySelectorAll(`.item.${chosen_food}`)
+    const make_visible = document.querySelectorAll(`.item.${chosen_filter}`)
     
-    currently_visible.forEach(function (food) {
-        food.classList.remove('active');
+    currently_visible.forEach(function (category) {
+        category.classList.remove('active');
 
-    make_visible.forEach(function (food) {
-        food.classList.add('active');
+    make_visible.forEach(function (category) {
+        category.classList.add('active');
     });
         
     });
